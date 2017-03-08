90ac5386e5b9d7535984946e0abcef7b2db3d31a
Swall0w <technext.jpn@gmail.com>
2017-03-08 19:36:07 +0900

    add md5 func.

--- check_file_volume.py
+++ check_file_volume.py
@@ -6,12 +6,15 @@ import hashlib
 def add_file_value(func,filedir,filelist):
     return {item : func(filedir+item) for item in filelist}
 
-#    return {item : os.path.getsize(filedir+item) for item in filelist}
-def get_filesize(x):
-    return os.path.getsize(x)
+def get_filesize(filepath):
+    return os.path.getsize(filepath)
 
-def get_file_md5(filedir, filelist):
-    pass
+def get_file_md5(filepath):
+    md5 = hashlib.md5()
+    with open(filepath, 'rb') as f:
+        for chunk in iter(lambda: f.read(2048 * md5.block_size), b''):
+            md5.update(chunk)
+    return  md5.hexdigest()
 
 def check_overlappedfile_by_filesize():
     filedir1 = 'csvdatav1/'
@@ -19,14 +22,15 @@ def check_overlappedfile_by_filesize():
     filelist1 = os.listdir(filedir1)
     filelist2 = os.listdir(filedir2)
 
-    filedict1 = add_file_value(get_filesize, filedir1, filelist1)
-    filedict2 = add_file_value(get_filesize, filedir2, filelist2)
+    filedict1 = add_file_value(get_file_md5, filedir1, filelist1)
+    filedict2 = add_file_value(get_file_md5, filedir2, filelist2)
 
     for key1, value1 in filedict1.items():
         for key2, value2 in filedict2.items():
             if value1 == value2:
                 print("Maybe {file1} in csvdatav1 is duplicated as {file2} in csvdatav2.".format(\
                 file1=key1, file2=key2))
+                os.remove(filedir2+key2)
 
 if __name__ == '__main__':
     check_overlappedfile_by_filesize()
