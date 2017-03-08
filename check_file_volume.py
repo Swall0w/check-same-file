bba868d6f5c9c2b6e5888bb9ab785d93b585f237
Swall0w <technext.jpn@gmail.com>
2017-03-08 15:46:20 +0900

    modified.

--- check_file_volume.py
+++ check_file_volume.py
@@ -5,7 +5,7 @@ import os.path
 def get_file_volume(filedir,filelist):
     return {item : os.path.getsize(filedir+item) for item in filelist}
 
-def main():
+def check_overlappedfile_by_filesize():
     filedir1 = 'csvdatav1/'
     filedir2 = 'csvdatav2/'
     filelist1 = os.listdir(filedir1)
@@ -15,17 +15,10 @@ def main():
     filedict2 = get_file_volume(filedir2,filelist2)
 
     for key1, value1 in filedict1.items():
-        if value1 in filedict2.values():
-            print("Maybe {filename} is duplicated.".format(filename=key1))
-#
-#    for k, v in filedict1.items():
-#        print(k, v)
-#
-#    for k in filedict1.keys():
-#        print('key: ', k)
-#
-#    for v in filedict1.values():
-#        print('value: ', v)
-#
+        for key2, value2 in filedict2.items():
+            if value1 == value2:
+                print("Maybe {file1} in csvdatav1 is duplicated as {file2} in csvdatav2.".format(\
+                file1=key1, file2=key2))
+
 if __name__ == '__main__':
-    main()
+    check_overlappedfile_by_filesize()
